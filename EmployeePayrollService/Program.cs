3f98645c0aae71c7f6c4d155d823f0aa5a2f9035
Prajwal <prajwal.zurunge12@gmail.com>
2021-11-14 16:44:01 +0530

    adding database retrieving program

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -6,7 +6,12 @@ namespace EmployeePayrollService
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to payroll services using ADO");
+            //Console.WriteLine("Welcome to payroll services using ADO");
+
+            EmployeeRepo repository = new EmployeeRepo();
+            Console.WriteLine("Id Name BasicPay Startdate Gender Department PhoneNumber Address Deduction Tax IncomeTax NetPay \n");
+            repository.GetAllEmployee();
+            Console.ReadLine();
         }
     }
 }
