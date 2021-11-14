67aa3e37505ac9a0cd0e27fd2748d95bbfc2e93f
Prajwal <prajwal.zurunge12@gmail.com>
2021-11-14 14:41:10 +0530

    first commit

---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to payroll services using ADO");
+        }
+    }
+}
